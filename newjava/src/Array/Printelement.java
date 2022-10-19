d472bc3bdb8208dccabebc0eb9329aab94fd6c95
Anand kumar pal <105219902+anandkumarapl@users.noreply.github.com>
2022-10-19 09:30:12 +0530

    Array problems

---
+++ newjava/src/Array/Printelement.java
@@ -0,0 +1,12 @@
+
+package Array;
+
+public class Printelement {
+    public static void main(String args[]){
+        int a[]={1,3,2,4,5,6,7};
+        for(int i=0;i<=a.length-1;i++){
+            System.out.print(a[i]);
+        }
+    }
+    
+}
